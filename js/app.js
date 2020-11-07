89baf25711275f0065624e2b8c1dbd4e8d45af2e
Brian Green <dear.briangreen@gmail.com>
2020-11-06 23:54:02 -0800

    Added logo
    Added logo with ternary condition at line 10.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ const Highlight = ({children, color}) => (
   </span>
 );
 
-const NavItem = ({className, href, children}) => (
+const NavItem = ({className, href, children, logo}) => (
   <li className={`mh2-ns f6 f4-l tc ${className}`}>
     <a className="white no-underline" href={href}>
-      {children}
+      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
     </a>
   </li>
   );
